cb07a1639082dd6fa0e233dd31dac6f66e1bf821
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-02-23 19:17:11 -0800

    send request to letterboxd, response not working though.

--- testing.ts
+++ testing.ts
@@ -8,3 +8,15 @@ console.log("hello world!");
 
 const apiToken = process.env.LETTERBOXD_API_TOKEN;
 const clientSecret = process.env.LETTERBOXD_CLIENT_SECRET;
+
+
+async function main(){
+    if (apiToken && clientSecret){
+        const client = new Client(apiToken, clientSecret);
+        const {status, data} = await client.film.get('ljDs');
+        console.log(data.name);
+        console.log(data.tagline);
+    }
+}
+
+main()
