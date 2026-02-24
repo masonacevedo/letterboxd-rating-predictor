965044c940cb9d16599631360e6896c301ae0a9c
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-02-23 19:25:18 -0800

    write data to file

--- testing.ts
+++ testing.ts
@@ -1,22 +1,34 @@
 import Client from 'letterboxd-client';
 import * as dotenv from "dotenv"
 
-dotenv.config()
-
-console.log("hello world!");
+import { writeFile } from 'fs/promises';
 
 
-const apiToken = process.env.LETTERBOXD_API_TOKEN;
-const clientSecret = process.env.LETTERBOXD_CLIENT_SECRET;
-
-
-async function main(){
-    if (apiToken && clientSecret){
-        const client = new Client(apiToken, clientSecret);
-        const {status, data} = await client.film.get('ljDs');
-        console.log(data.name);
-        console.log(data.tagline);
-    }
+async function saveJson(fileName: string, jsonData: unknown){
+    await writeFile(fileName, JSON.stringify(jsonData, null, 2));
 }
 
-main()
+const myData = {1:2, "marco":"polo"};
+
+saveJson("bar.txt", myData);
+
+
+// dotenv.config()
+
+// console.log("hello world!");
+
+
+// const apiToken = process.env.LETTERBOXD_API_TOKEN;
+// const clientSecret = process.env.LETTERBOXD_CLIENT_SECRET;
+
+
+// async function main(){
+//     if (apiToken && clientSecret){
+//         const client = new Client(apiToken, clientSecret);
+//         const {status, data} = await client.film.get('ljDs');
+//         console.log(data.name);
+//         console.log(data.tagline);
+//     }
+// }
+
+// main()
