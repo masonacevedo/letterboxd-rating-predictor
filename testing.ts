52fe8284f9489af7b7fe223f6ab70b9150c53561
Mason Acevedo <mason.acevedo2000@gmail.com>
2026-02-23 18:58:04 -0800

    access secrets!

--- testing.ts
+++ testing.ts
@@ -1,3 +1,10 @@
 import Client from 'letterboxd-client';
+import * as dotenv from "dotenv"
+
+dotenv.config()
 
 console.log("hello world!");
+
+
+const apiToken = process.env.LETTERBOXD_API_TOKEN;
+const clientSecret = process.env.LETTERBOXD_CLIENT_SECRET;
